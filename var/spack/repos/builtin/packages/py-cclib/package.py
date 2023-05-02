064b960d5906c41486decb11caf256f2bda83cea
amandadumi <amanda.e.dumi@gmail.com>
2023-05-02 16:49:48 -0600

    basic changes according to docs, untested

--- var/spack/repos/builtin/packages/py-cclib/package.py
+++ var/spack/repos/builtin/packages/py-cclib/package.py
@@ -13,6 +13,10 @@ class PyCclib(PythonPackage):
     homepage = "https://cclib.github.io/"
 
     version(
+        "1.7.2",
+        url = "https://github.com/cclib/cclib/releases/download/v1.7.2/cclib-1.7.2.tar.gz",
+        sha256="aa1a2b2e26f7debbd98605d471d4dafe8cacf42db76b961df79b8cb29b2f3cdc"
+    version(
         "1.5.post1",
         sha256="c2bf043432ab8df461d61b4289d0eb869fe134eee545ea5a78f8dea14b392f47",
         url="https://github.com/cclib/cclib/releases/download/v1.5/cclib-1.5.post1.tar.gz",
@@ -21,3 +25,8 @@ class PyCclib(PythonPackage):
     # pip silently replaces distutils with setuptools
     depends_on("py-setuptools", type="build")
     depends_on("py-numpy@1.5:", type=("build", "run"))
+    depends_on("periodictable", type=("build", "run"),when="@1.7.2:")
+    depends_on("scipy@1.2.0:", type=("build", "run"),when="@1.7.2:")
+
+    # maintainers
+    maintainers=["amandadumi","berquist"]
